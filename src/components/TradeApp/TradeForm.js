13a1b5aee8401a555f3ba885179c04e101164a5d
Matt <m23.cullen@gmail.com>
2020-09-02 16:45:49 -0400

    add psudo code for tradeform.js

--- src/components/TradeApp/TradeForm.js
+++ src/components/TradeApp/TradeForm.js
@@ -12,6 +12,8 @@ import {
   Divider
 } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import FileModal from '../ChatApp/Messages/FileModal';
+import ProgressBar from '../ChatApp/Messages/ProgressBar';
 
 class TradeForm extends React.Component {
   state = {
@@ -35,27 +37,12 @@ class TradeForm extends React.Component {
     this.setState({ [event.target.name]: event.target.value });
   };
 
+
   handleSubmit = event => {
-    event.preventDefault();
-    if (this.isFormValid(this.state)) {
-      this.setState({
-        errors: [],
-        loading: true
-      });
-      firebase
-        .auth()
-        .signInWithEmailAndPassword(this.state.email, this.state.password)
-        .then(signedInUser => {
-          console.log(signedInUser);
-        })
-        .catch(err => {
-          console.error(err);
-          this.setState({
-            errors: this.state.errors.concat(err),
-            loading: false
-          });
-        });
-    }
+    // sends a post to firestore
+    // must get a post object with createPost
+    // check for an image
+    // if there is an image use the uploadfile function, attach a url to the post object for future rendering.
   };
 
   isFormValid = ({ idol, group, comment }) => idol || group && comment
@@ -67,6 +54,11 @@ class TradeForm extends React.Component {
         ? "error"
         : ""
   }
+
+  createPost = () => {
+    //takes state and returns object for sending to firestore
+  }
+
 
   // Pass this to the file modal and customize it for images associated with trades.
   uploadFile = (file, metadata) => {
@@ -167,7 +159,7 @@ class TradeForm extends React.Component {
                 icon="cloud upload"
               />
 
-              {/* <FileModal 
+              <FileModal 
                 modal={modal}
                 closeModal={this.closeModal}
                 uploadFile={this.uploadFile}
@@ -175,7 +167,7 @@ class TradeForm extends React.Component {
               <ProgressBar 
                 uploadState={uploadState} 
                 percentUploaded={percentUploaded}
-              /> */}
+              />
               
               <Button disabled={loading} className={loading ? 'loading' : ''} color='violet' fluid size='large'>Post</Button>
             </Segment>
