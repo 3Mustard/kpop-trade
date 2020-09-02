7a414fb278274b46ab005bb022b276a60ec4f482
3Mustard <m23.cullen@gmail.com>
2020-09-01 20:18:47 -0400

    added uploadFile function for tradeimages

--- src/components/TradeApp/TradeForm.js
+++ src/components/TradeApp/TradeForm.js
@@ -68,6 +68,49 @@ class TradeForm extends React.Component {
         : ""
   }
 
+  // Pass this to the file modal and customize it for images associated with trades.
+  uploadFile = (file, metadata) => {
+    // const pathToUpload = this.state.channel.id;
+    // const ref = this.props.getMessagesRef();
+    // const filePath = `${this.getPath()}/${uuidv4()}.jpg`;
+
+    // this.setState({
+    //   uploadState: 'uploading',
+    //   uploadTask: this.state.storageRef.child(filePath).put(file, metadata)
+    // },
+    //   () => {
+    //     this.state.uploadTask.on(
+    //       'state_changed', 
+    //       snap => {
+    //         const percentUploaded = Math.round((snap.bytesTransferred / snap.totalBytes) * 100);
+    //         this.setState({ percentUploaded }); 
+    //     },
+    //       err => {
+    //         console.error(err);
+    //         this.setState({
+    //           errors: this.state.errors.concat(err),
+    //           uploadState: 'error',
+    //           uploadTask: null
+    //         })
+    //       },
+    //       () => {
+    //         this.state.uploadTask.snapshot.ref.getDownloadURL().then(downloadUrl => {
+    //           this.sendFileMessage(downloadUrl, ref, pathToUpload);
+    //         })
+    //         .catch(err => {
+    //           console.error(err);
+    //           this.setState({
+    //             errors: this.state.errors.concat(err),
+    //             uploadState: 'error',
+    //             uploadTask: null
+    //           })
+    //         })
+    //       }
+    //     )
+    //   }
+    // )
+  };
+
   render() {
     const { idol, group, comment, errors, loading, uploadState, percentUploaded } = this.state;
 
