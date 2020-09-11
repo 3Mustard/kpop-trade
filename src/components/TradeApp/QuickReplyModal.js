f70380cfd3dafd29d1931abe685c913111a97ac1
3Mustard <m23.cullen@gmail.com>
2020-09-10 20:57:02 -0400

    createmessage function and getuniquechatid function added

--- src/components/TradeApp/QuickReplyModal.js
+++ src/components/TradeApp/QuickReplyModal.js
@@ -1,6 +1,7 @@
 // form for sending a message from trades component
 
 import React, { Component } from 'react'
+import uuidv4 from 'uuid/v4';
 import firebase from '../../firebase';
 import { Form } from 'semantic-ui-react'
 
@@ -20,13 +21,40 @@ class QuickReplyModal extends Component {
     this.props.resetAfterSubmit();
   }
 
-  sendReply = () => {
+  createMessage = () => {
+    const message = {
+      timestamp: firebase.database.ServerValue.TIMESTAMP,
+      user: {
+        id: this.state.user.uid,
+        name: this.state.user.displayName,
+        avatar: this.state.user.photoURL
+      },
+      content: this.state.content
+    }
+    return message;
+  }
 
+  sendReply = () => {
+    const { content, recipient, user, chatsRef, usersRef } = this.state;
+    const chatId = this.getUniqueChatId();
+    const id1 = chatId.split('-')[0];
+    const id2 = chatId.split('-')[1];
+
+    chatsRef
+      .child(chatId)
+      .push()
+      .set(this.createMessage())
+      
+  }
+
+  getUniqueChatId = () => {
+    const currentUserId = this.state.user.uid;
+    const recipientId = this.state.recipient.id;
+
+    return recipientId < currentUserId ? `${recipientId}-${currentUserId}` : `${currentUserId}-${recipientId}`;
   }
 
   render() {
-    const { handleSubmit } = this.props;
-
     return (
       <Form>
         <Form.TextArea onChange={this.handleChange} name='content' value={this.state.content} label='Reply' placeholder='Reply to poster...' />
@@ -37,3 +65,38 @@ class QuickReplyModal extends Component {
 }
 
 export default QuickReplyModal;
+
+// sendMessage = () => {
+//   const { getMessagesRef } = this.props;
+//   const { message, channel, user, typingRef } = this.state;
+
+//   if (message) {
+//     this.setState({ loading: true });
+//     getMessagesRef()
+//       .child(channel.id)
+//       .push()
+//       .set(this.createMessage())
+//       .then(() => {
+//         this.setState({ 
+//           loading: false, 
+//           message: '', 
+//           errors: [] 
+//         });
+//         typingRef
+//           .child(channel.id)
+//           .child(user.uid)
+//           .remove();
+//       })
+//       .catch(err => {
+//         console.error(err);
+//         this.setState({
+//           loading: false,
+//           errors: this.state.errors.concat(err)
+//         })
+//       })
+//   } else {
+//     this.setState({
+//       errors: this.state.errors.concat({ message: 'Add a message' })
+//     })
+//   }
+// };
