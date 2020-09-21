9e9abd93bdbb97d2616d9e96d2811d2f73877b26
3Mustard <m23.cullen@gmail.com>
2020-09-21 10:17:24 -0400

    set up lifecycle methods to track window size

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,30 @@ import TopBar from './Layout/TopBar';
 import FooterMenu from './Layout/FooterMenu';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      windowWidth: 0,
+      windowHeight: 0
+    };
+    this.updateDimensions = this.updateDimensions.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateDimensions();
+    window.addEventListener("resize", this.updateDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions);
+  }
+
+  updateDimensions() {
+    let windowWidth = typeof window !== "undefined" ? window.innerWidth : 0;
+    let windowHeight = typeof window !== "undefined" ? window.innerHeight : 0;
+
+    this.setState({ windowWidth, windowHeight });
+  }
 
   render() {
     const styles = {
